8fa09d80b393e2016a155eba36f5d1901391f827
KJ Jackson <kjjackson619@yahoo.com>
2021-12-21 18:48:34 -0500

    generate webpage

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
+const profileDataArgs = process.argv.slice(2);
 
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('=========');
-
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
